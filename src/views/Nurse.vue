53b9001284b474d8aef05f27fb4cbe91d2fe5dec
Abdurasul <rakhimovabdurasul@gmail.com>
2019-04-19 21:27:48 +0300

    reports fetched from database

--- src/views/Nurse.vue
+++ src/views/Nurse.vue
@@ -18,13 +18,11 @@
         <v-data-table :headers="headers" :items="rooms" :search="search">
           <template v-slot:items="props">
             <td>{{ props.item.roomId }}</td>
-            <!-- <td class="text-xs-right">{{ props.item.roomId }}</td> -->
-            <td>{{ props.item.bedId }}</td>
+            <td>{{ props.item.roomType }}</td>
             <td>{{ props.item.patientId }}</td>
-            <td>{{ props.item.pName }}</td>
-            <!-- <td>{{ props.item.protein }}</td> -->
+            <!-- <td>{{ props.item.pName }}</td> -->
             <td>
-              <v-btn round dark ripple>delete</v-btn>
+              <v-btn v-on:click="remove(props.item.roomId)" round dark ripple>delete</v-btn>
             </td>
           </template>
           <v-alert
@@ -39,7 +37,31 @@
 
     <v-tab-item>
       <v-card flat>
-        <v-card-text>Contents for Item 2 go here</v-card-text>
+        <v-card-title>
+          Reports
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers2" :items="reports" :search="search2">
+          <template v-slot:items="props">
+            <td>{{ props.item.testT }}</td>
+            <td>{{ props.item.testR }}</td>
+            <td>{{ props.item.laboratorist }}</td>
+            <td>{{ props.item.patient }}</td>
+          </template>
+          <v-alert
+            v-slot:no-results
+            :value="true"
+            color="error"
+            icon="warning"
+          >Your search for "{{ search2 }}" found no results.</v-alert>
+        </v-data-table>
       </v-card>
     </v-tab-item>
   </v-tabs>
@@ -47,20 +69,26 @@
 
 <script>
 import firebase from 'firebase'
-//import firebaseConfig from './firebaseConfig'
-
-//const firebaseApp = firebase.initializeApp(firebaseConfig)
-
-//export default firebaseApp.firestore() 
-//import db from "./firebaseinit";
 
 var db = firebase.firestore()
 
 export default {
   data() {
     return {
-      // callback: function(){
-      // },
+      remove: function(id) {
+        var deleteID = 0;
+        // console.log(db.collection('patients').doc('p1'))
+        db.collection('rooms').get().then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+              if(doc.data().roomID === id){
+                deleteID = doc.id
+                db.collection('rooms').doc(deleteID).delete()
+              }
+          })
+        })
+        
+        
+      },
       search: "",
       headers: [
         {
@@ -69,63 +97,54 @@ export default {
           sortable: true,
           value: "roomId"
         },
-        { text: "Bed ID", value: "bedId" },
+        { text: "Type of room", value: "roomType" },
         {
           text: "Patient ID",
           value: "patientId"
         },
-        { text: "Patient Name", value: "pName" },
+        // { text: "Patient Name", value: "pName" },
         { text: "Edit Info", value: "edit" }
       ],
-      rooms: [
-        // {
-        //   roomId: 1,
-        //   bedId: 1,
-        //   patientId: 1,
-        //   pName: "John Doe",
-        //   protein: 4.0
-        // },
-        // {
-        //   roomId: 1,
-        //   bedId: 1,
-        //   patientId: 1,
-        //   pName: "John Doe",
-        //   protein: 4.0
-        // },
-        // {
-        //   roomId: 1,
-        //   bedId: 1,
-        //   patientId: 1,
-        //   pName: "John Doe",
-        //   protein: 4.0
-        // },
-        // {
-        //   roomId: 1,
-        //   bedId: 1,
-        //   patientId: 1,
-        //   pName: "John Doe",
-        //   protein: 4.0
-        // }
+    
+      rooms: [],
+    
+      search2: "",
+      headers2: [
+        { text: "Test Type", value: "testT" },
+        { text: "Test Type", value: "testR" },
+        { text: "Laboratorist", value: "laboratorist" },
+        { text: "Patient", value: "patient" }
       ],
+      reports: [],
     };
   },
   created () {
       console.log(this.$route.name)
+      console.log(db.collection('rooms').doc('1'))
       db.collection('rooms').get().then(querySnapshot => {
           querySnapshot.forEach(doc => {
-              console.log(doc.data())
+              var pID = doc.data().patient.id
               const data = {
-                //   'roomId': doc.data().roomId,
                 'roomId': doc.data().roomID,
-                'bedId': doc.data().roomID,
-                'patientId': "1",
-                'pName': "John Doe",
-                // 'protein': doc.data().patient
-                
+                'roomType': doc.data().roomType,
+                'patientId': doc.data().patient.id,
+                // 'pName': "John Doe" //doc.data().patient.get().data().name,
               }
               this.rooms.push(data)
           })
       })
+      db.collection('reports').get().then(querySnapshot =>{
+        querySnapshot.forEach(doc => {
+          const data = {
+            'laboratorist': doc.data().laboratorist.id,
+            'patient': doc.data().patient.id,
+            'testT': doc.data().testT,
+            'testR': doc.data().testR
+          
+          }
+          this.reports.push(data)
+        })
+      }) 
   }
 };
 </script>
